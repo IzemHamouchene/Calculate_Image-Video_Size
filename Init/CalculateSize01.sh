a0d9ead9d6792743db0ad6dd8a7e3b4d9631fa5d
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2022-01-18 12:46:56 +0100

    Create CalculateSize01.sh

---
+++ Init/CalculateSize01.sh
@@ -0,0 +1,57 @@
+#===================== TEST 1 ====================
+echo "====== TEST 1 "
+echo "Image size : 1024 X 1024"
+#Init data
+outputEXE=$(./exo1.out "1024" "1024")
+Correct_Output_Array=("1024.00" "3.00")
+
+Output_Array=($outputEXE)
+
+#for i in ${Output_Array[*]}; do
+#  echo $i
+#done
+
+# Test Length output
+if [ ${#Correct_Output_Array[@]} -ne ${#Output_Array[@]} ]; then
+       echo "Expected ${#Correct_Output_Array[@]} output lines but got ${#Output_Array[@]} lines"
+       exit 1
+fi
+
+# Test Array output
+for (( i="0"; i<${#Correct_Output_Array[@]}; i++ ))
+do
+       if [ ${Correct_Output_Array[${i}]} != ${Output_Array[${i}]} ]; then
+              echo "Expected '${Correct_Output_Array[${i}]}' but got : '${Output_Array[${i}]}'"
+              exit 1
+       fi
+done
+
+#===================== TEST 2 ====================
+echo "====== TEST 2 "
+echo "Image size : 1024 X 1000"
+#Init data
+outputEXE=$(./exo1.out "1024" "1000")
+Correct_Output_Array=("1000.00" "2.92")
+
+Output_Array=($outputEXE)
+
+#for i in ${Output_Array[*]}; do
+#  echo $i
+#done
+
+# Test Length output
+if [ ${#Correct_Output_Array[@]} -ne ${#Output_Array[@]} ]; then
+       echo "Expected ${#Correct_Output_Array[@]} output lines but got ${#Output_Array[@]} lines"
+       exit 1
+fi
+
+# Test Array output
+for (( i="0"; i<${#Correct_Output_Array[@]}; i++ ))
+do
+       if [ ${Correct_Output_Array[${i}]} != ${Output_Array[${i}]} ]; then
+              echo "Expected '${Correct_Output_Array[${i}]}' but got : '${Output_Array[${i}]}'"
+              exit 1
+       fi
+done
+echo "All tests passed."
+exit 0

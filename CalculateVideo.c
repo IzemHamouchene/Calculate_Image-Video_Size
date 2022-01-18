87174898ee8c93e0c3c74a6fd82266462c1e3710
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2022-01-18 18:14:59 +0100

    Update and rename Input_output.c to CalculateVideo.c

---
+++ CalculateVideo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+   // Param 1 : Size of one image (gray level)
+   // Param 2 : Video length
+   // Param 2 : FPS
+   
+   // Output 1 : Video Size
+   return 0;
+}
--- Input_output.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-   //Affichage sous cette forme
-   //printf("%s\n", XXXXX);
-   return 0;
-}
